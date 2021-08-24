476b975aefa87c7d9d7c04a9794f17e965e11bbf
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-08-24 15:48:36 -0400

    Added error handling

--- routes/users.ts
+++ routes/users.ts
@@ -5,6 +5,7 @@ import * as _ from "lodash"
 import { userModel } from "../models/models"
 import { validateBody , validateSignupBody} from "../utils/validationsFunctions"
 
+
 const router = express.Router()
 
 interface SignUpBody {
@@ -17,10 +18,23 @@ router.post("/", async (req: any, res: any, next: Function) => {
     const isValid = await validateSignupBody(req,res)
     if (!isValid) return 
     const {username, password, email } = req.body
-    const user = new userModel({email, password, username})
-    const response = await user.save()
-    res.send(_.pick(response, ["username", "email"]))
+
+    const encryptedPassword = await encryptPassword(password)
+    const user = new userModel({email, password: encryptedPassword, username})
+
+    try {
+        const response = await user.save()
+        res.send(_.pick(response, ["username", "email"]))
+    }
+    catch(err) {
+        res.status(503).send("There was an error")
+    }
 })
+
+async function encryptPassword(password: string) : Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt)
+}   
 
 
 
