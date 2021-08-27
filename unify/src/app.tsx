03ade0b55b221af60c4ed01cc9c51b954556de73
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-08-26 21:50:13 -0400

    create main app component

---
+++ unify/src/app.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Route } from "react-router-dom"
+
+export default class App extends Component  {
+    state = {}
+
+    render() {
+       return (<div></div>)
+    }
+}
