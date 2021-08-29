ba7d8378635056612eb52cb71e92cd88acafda3c
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-08-29 19:26:56 -0400

    added routing to home instead of login after signup

--- unify/src/components/SignupPage/SignupPage.tsx
+++ unify/src/components/SignupPage/SignupPage.tsx
@@ -1,31 +1,38 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import SignupForm, {SignupData} from './SignupForm'
 import Navbar from '../Navbar'
 import { toast } from 'react-toastify'
 import axios from 'axios'
+import tokenContext from '../../contexts/tokenContext'
 toast.configure()
 
 
 const SignupPage = (props: RouteComponentProps) => {
+    const { setToken } = useContext(tokenContext)
     return (
         <Fragment>
             <Navbar />
              <div className = "signup-page-wrapper">
-            <SignupForm onSubmit={SubmitSignup}/>
+            <SignupForm onSubmit={async (data: SignupData) => {
+                const response = await axios.post("http://localhost:3000/users", data)
+                if (response.status !== 200) {
+                    toast.error("There was an error")
+                    return new Promise((resolve, reject) => resolve(undefined))
+                } 
+            
+                const { user_auth_token } = response.data
+                setToken(user_auth_token)
+                console.log("set token ")
+                toast.info("Account created successfuly")
+                props.history.push("/")
+            }}/>
             </div>
         </Fragment>
     )
 }
 
-async function SubmitSignup(data: SignupData) : Promise<void> {
-    const response = await axios.post("http://localhost:3000/users", data)
-    if (response.status !== 200) {
-        toast.error("There was an error")
-        return new Promise((resolve, reject) => resolve(undefined))
-    } 
-    toast.info("Account created successfuly")
-    window.location.href = "/login"
-}
+
+
 
 export default SignupPage
